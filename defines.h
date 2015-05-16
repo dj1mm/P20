cfa97da5ce751f80297a5be78161dc11486e1bca
Jimmy <j.ahfat95@gmail.com>
2015-05-16 22:02:49 +0100

    Create defines.h

---
+++ defines.h
@@ -0,0 +1,17 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <QDebug>
+
+#define BITLENGTH 7
+
+#define MANAGER_BITHEADER 3
+
+#define TRANSFER_DELAY 10 // Delay between pin toggles (to allow mutex locking and unlocking at the reading process)
+
+// #define DEBUG_APPLICATION
+// #define DEBUG_MANAGER
+// #define DEBUG_NETWORK
+#define DEBUG_PHYSICAL
+
+#endif // DEFINES_H
