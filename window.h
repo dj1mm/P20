ba4d29a8c759d605bcfba172e18dfc587eff5c43
Jimmy <j.ahfat95@gmail.com>
2015-05-16 21:57:22 +0100

    Create window.h

---
+++ window.h
@@ -0,0 +1,28 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+/* 
+
+This code was provided as starting code. Code not used.
+
+#include <QMainWindow>
+
+namespace Ui {
+class Window;
+}
+
+class Window : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit Window(QWidget *parent = 0);
+    ~Window();
+    
+private:
+    Ui::Window *ui;
+};
+
+*/
+
+#endif // WINDOW_H
