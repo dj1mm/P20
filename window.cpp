5ec6aa0576619d8fe079ff3ca9934f8708e4ccfb
Jimmy <j.ahfat95@gmail.com>
2015-05-16 22:11:05 +0100

    Create window.cpp

---
+++ window.cpp
@@ -0,0 +1,19 @@
+#include "window.h"
+/*
+
+Code provided as sample code. Did not used this code. See mywindow.h or .cpp
+
+#include "ui_window.h"
+
+Window::Window(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Window)
+{
+    ui->setupUi(this);
+}
+
+Window::~Window()
+{
+    delete ui;
+}
+*/
